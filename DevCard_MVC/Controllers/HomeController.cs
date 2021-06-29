e18b10dcf695c2f6fc100782b396d9911a205489
Mardibak <mardibaki@gmail.com>
2021-06-29 12:13:16 +0430

    index & contact page added

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -11,11 +11,9 @@ namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+
         }
 
         public IActionResult Index()
@@ -23,10 +21,11 @@ namespace DevCard_MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
