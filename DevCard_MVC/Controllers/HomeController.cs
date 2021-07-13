76b5997eaf4dd39584f0ef1656b5795c16456464
mardibak <mardibaki@gmail.com>
2021-07-13 11:44:17 +0430

    Service and Color added to project

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        private readonly List<Service> _services = new List<Service>
         {
+            new Service(1, "Silver"),
+            new Service(2, "Plutin"),
+            new Service(3, "Gold"),
+            new Service(4, "Dimon")
+        };
 
-        }
+        private readonly List<Color> _colors = new List<Color>
+        {
+            new Color(1, "Red"),
+            new Color(2,"Blue"),
+            new Color(3,"Yellow"),
+            new Color(4, "Black")
+        };
 
         public IActionResult Index()
         {
@@ -24,28 +32,34 @@ namespace DevCard_MVC.Controllers
         [HttpGet]
         public IActionResult Contact()
         {
-            var model = new Contact();
+            var model = new Contact
+            {
+                 Services = new SelectList(_services, "Id","Name"),
+                 Colors = new SelectList(_colors,"Id","Name")
+            };
             return View(model);
+
         }
-
-        //Recive Information From Contact Form
-        //[HttpPost]
-        //public JsonResult Contact(IFormCollection form)
-        //{
-        //    var name = form["name"];
-        //    return Json(Ok());
-        //}
-
-
 
         //Other Methode for Give Information from Contact Form
         [HttpPost]
-        public JsonResult Contact(Contact form)
+        //Validation
+        public IActionResult Contact(Contact model)
         {
-            Console.WriteLine(form.ToString());
-            return Json(Ok());
+            model.Services = new SelectList(_services, "Id", "Name");
+            model.Colors = new SelectList(_colors, "Id", "Name");
+            if (!ModelState.IsValid)
+            {
+                //ViewData["error"]
+                ViewBag.error = "اطلاعات وارد شده صحیح نیست. دوباره تلاش کنید";
+                return View(model);
+            }
+            //Clear After Send
+            ModelState.Clear();
+            model = new Contact();
+            ViewBag.success = "پیام شما با موفقیت ارسال شد";
+            return View(model);
         }
-
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- DevCard_MVC/Models/Contact.cs
+++ DevCard_MVC/Models/Contact.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevCard_MVC.Models
 {
     public class Contact
     {
 
-        [Required(ErrorMessage ="پر کردن این فیلد اجباری است")]
-        [MinLength(4,ErrorMessage ="حداقل طول نام 4 کاراکتر است")]
-        [MaxLength(80,ErrorMessage ="حد اکثر طول نام ۸۰ کاراکتر است")]
+        [Required(ErrorMessage = "پر کردن این فیلد اجباری است")]
+        [MinLength(4, ErrorMessage = "حداقل طول نام 4 کاراکتر است")]
+        [MaxLength(80, ErrorMessage = "حد اکثر طول نام ۸۰ کاراکتر است")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "پر کردن این فیلد اجباری است")]
-        [EmailAddress(ErrorMessage ="ایمیل را به صورت صحیح وارد نمایید")]
+        [EmailAddress(ErrorMessage = "ایمیل را به صورت صحیح وارد نمایید")]
         public string Email { get; set; }
 
         public string Message { get; set; }
-        public string Service { get; set; }
+        public int Service { get; set; }
+        public int Color { get; set; }
+        public SelectList Services { get; set; }
+        public SelectList Colors { get; set; }
 
     }
 }
